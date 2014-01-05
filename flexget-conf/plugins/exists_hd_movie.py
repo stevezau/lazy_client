9ba1da11eb959edbe4e06862669e2275ec170e87
Steven Adams <stevezau@gmail.com>
2014-01-05 16:17:11 +1100

    .

---
+++ flexget-conf/plugins/exists_hd_movie.py
@@ -0,0 +1,139 @@
+from __future__ import unicode_literals, division, absolute_import
+import os
+import logging
+
+from flexget.config_schema import one_or_more
+from flexget.plugin import register_plugin, priority, PluginError, get_plugin_by_name
+from flexget.utils.titles.movie import MovieParser
+
+log = logging.getLogger('exists_HD_movie')
+
+
+class FilterExistsMovie(object):
+
+    """
+    Reject existing movies.
+
+    Example::
+
+      exists_movie: /storage/movies/
+    """
+
+    schema = one_or_more({'type': 'string', 'format': 'path'})
+
+    skip = ['cd1', 'cd2', 'subs', 'sample']
+
+    def __init__(self):
+        self.cache = {}
+
+    def build_config(self, config):
+        # if only a single path is passed turn it into a 1 element list
+        if isinstance(config, basestring):
+            config = [config]
+        return config
+
+    def on_process_start(self, task, config):
+        self.cache = {}
+
+    @priority(-1)
+    def on_task_filter(self, task, config):
+        if not task.accepted:
+            log.debug('nothing accepted, aborting')
+            return
+
+        config = self.build_config(config)
+        imdb_lookup = get_plugin_by_name('imdb_lookup').instance
+
+        incompatible_dirs = 0
+        incompatible_entries = 0
+        count_entries = 0
+        count_dirs = 0
+
+        # list of imdb ids gathered from paths / cache
+        imdb_ids = []
+
+        for path in config:
+            # see if this path has already been scanned
+            if path in self.cache:
+                log.verbose('Using cached scan for %s ...' % path)
+                imdb_ids.extend(self.cache[path])
+                continue
+
+            path_ids = []
+
+            # with unicode it crashes on some paths ..
+            path = str(os.path.expanduser(path))
+            if not os.path.exists(path):
+                log.critical('Path %s does not exist' % path)
+                continue
+
+            log.verbose('Scanning path %s ...' % path)
+
+            # Help debugging by removing a lot of noise
+            #logging.getLogger('movieparser').setLevel(logging.WARNING)
+            #logging.getLogger('imdb_lookup').setLevel(logging.WARNING)
+
+            # scan through
+            for root, dirs, files in os.walk(path):
+                # convert filelists into utf-8 to avoid unicode problems
+                dirs = [x.decode('utf-8', 'ignore') for x in dirs]
+                # files = [x.decode('utf-8', 'ignore') for x in files]
+
+                # TODO: add also video files?
+                for item in dirs:
+
+                    for filename in os.listdir(root + os.sep + item):
+                        fileName, fileExtension = os.path.splitext(root + os.sep + item +  filename)
+                        if fileExtension == ".avi":
+                            continue
+
+                    if item.lower() in self.skip:
+                        continue
+                    count_dirs += 1
+
+                    movie = MovieParser()
+                    movie.parse(item)
+
+                    try:
+                        imdb_id = imdb_lookup.imdb_id_lookup(movie_title=movie.name,
+                                                             raw_title=item,
+                                                             session=task.session)
+                        if imdb_id in path_ids:
+                            log.trace('duplicate %s' % item)
+                            continue
+                        if imdb_id is not None:
+                            log.trace('adding: %s' % imdb_id)
+                            path_ids.append(imdb_id)
+                    except PluginError as e:
+                        log.trace('%s lookup failed (%s)' % (item, e.value))
+                        incompatible_dirs += 1
+
+            # store to cache and extend to found list
+            self.cache[path] = path_ids
+            imdb_ids.extend(path_ids)
+
+        log.debug('-- Start filtering entries ----------------------------------')
+
+        # do actual filtering
+        for entry in task.accepted:
+            count_entries += 1
+            if not entry.get('imdb_id', eval_lazy=False):
+                try:
+                    imdb_lookup.lookup(entry)
+                except PluginError as e:
+                    log.trace('entry %s imdb failed (%s)' % (entry['title'], e.value))
+                    incompatible_entries += 1
+                    continue
+
+            # actual filtering
+            if entry['imdb_id'] in imdb_ids:
+                entry.reject('movie exists')
+
+        if incompatible_dirs or incompatible_entries:
+            log.verbose('There were some incompatible items. %s of %s entries '
+                        'and %s of %s directories could not be verified.' %
+                (incompatible_entries, count_entries, incompatible_dirs, count_dirs))
+
+        log.debug('-- Finished filtering entries -------------------------------')
+
+register_plugin(FilterExistsMovie, 'exists_hd_movie', groups=['exists'], api_ver=2)
---
+++ flexget-conf/plugins/lazy.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, division, absolute_import
+from urllib import quote
+from requests.exceptions import RequestException, HTTPError
+from logging import getLogger
+from flexget.utils import json, requests
+from flexget.plugin import register_plugin, PluginError
+from flexget import validator
+import re
+log = getLogger('lazy')
+
+
+
+class PluginLazy(object):
+    """
+    Parse task content or url for links and adds them to lazy.
+    """
+
+    __author__ = 'steve'
+    __version__ = '0.1'
+
+    DEFAULT_API = 'http://localhost/lazy/api'
+    DEFAULT_PENDING = False
+    DEFAULT_HANDLE_NO_URL_AS_FAILURE = False
+
+    def __init__(self):
+        self.session = None
+
+    def validator(self):
+        """Return config validator"""
+        root = validator.factory()
+        root.accept('boolean')
+        advanced = root.accept('dict')
+        advanced.accept('text', key='api')
+        advanced.accept('boolean', key='pending')
+        return root
+
+    def on_process_start(self, task, config):
+        self.session = None
+
+    def on_task_output(self, task, config):
+        if not config.get('enabled', True):
+            return
+        if not task.accepted:
+            return
+
+        self.add_entries(task, config)
+
+    def add_entries(self, task, config):
+        """Adds accepted entries"""
+
+        try:
+            self.check_up(task, config)
+        except IOError:
+            raise PluginError('lazy not reachable', log)
+        except PluginError:
+            raise
+        except Exception as e:
+            raise PluginError('Unknown error: %s' % str(e), log)
+
+        api_url = config.get('api', self.DEFAULT_API)
+        pending = config.get('pending', self.DEFAULT_PENDING)
+
+        f = open('/tmp/lazy.log','a')
+
+        import datetime
+
+        for entry in task.accepted:
+
+            log.debug("Add %s to lazy" % entry['path'])
+
+            curtime = datetime.date.today().strftime("%B %d, %Y")
+
+            f.write("\n\n adding release date: %s  path: %s \n" % (str(curtime), entry['path']))
+
+            imdb_id = None
+            tvdb_id = None
+
+            if 'imdb_id' in entry:
+                 imdb_id = extract_id(entry['imdb_id'])
+
+            if 'tvdb_id' in entry:
+                 tvdb_id = int(entry['tvdb_id'])
+
+            status = 1
+
+            if pending:
+                status = 6
+
+            try:
+                post = {'ftppath': "%s" % entry['path'],
+                        'imdbid_id': imdb_id,
+                        'tvdbid_id': tvdb_id,
+                        'status': status,
+                    }
+
+                f.write("\n\n %s \n\n" % str(post))
+
+                pid = query_api(api_url, "downloads", post).text
+                log.info('added entry to lazy %s' % entry['path'])
+
+            except Exception as e:
+                log.exception(e)
+                entry.fail(str(e))
+
+        f.close()
+
+    def check_up(self, task, config):
+        url = config.get('api', self.DEFAULT_API).rstrip("/") + "/"
+
+        try:
+            query_api(url, 'downloads')
+        except HTTPError as e:
+            raise PluginError('HTTP Error %s' % e, log)
+
+def query_api(url, method, post=None):
+    try:
+        response = requests.request(
+            'post' if post is not None else 'get',
+            url.rstrip("/") + "/" + method.strip("/") + "/",
+            data=post)
+        response.raise_for_status()
+        return response
+    except RequestException as e:
+        log.debug(e.response)
+        if e.response.status_code == 500:
+            raise PluginError('Internal API Error: <%s> <%s> <%s>' % (method, url, post), log)
+        raise
+
+register_plugin(PluginLazy, 'lazy', api_ver=2)
+
+def extract_id(url):
+    """Return IMDb ID of the given URL. Return None if not valid or if URL is not a string."""
+    if not isinstance(url, basestring):
+        return
+    m = re.search(r'(?:nm|tt([\d]{7}))', url)
+    if m:
+        return m.group(1)
